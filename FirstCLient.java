8cc38fe657a2db9426b05ea653b295630143c6a5
mskrzypk <mskrzypk@mion.elka.pw.edu.pl>
2017-11-17 19:52:52 +0100

    finished rewriting CLient

--- FirstCLient.java
+++ FirstCLient.java
@@ -1,7 +1,11 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.Socket;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -36,27 +40,68 @@ public class FirstCLient {
 			}
 		});
     }
+    
+    private String getServerAdress() 
+    {
+    	return JOptionPane.showInputDialog(frame,  "Enter IP of the Server", "Welcome to the Client Chatter", JOptionPane.QUESTION_MESSAGE);
+    }
+    
+    private String getName()
+    {
+    	return JOptionPane.showInputDialog(frame,  "Enter IP of the Server", "Welcome to the Client Chatter", JOptionPane.PLAIN_MESSAGE);
+    }
+    
+    private void run() throws IOException
+    {
+    	String serverAddress = getServerAdress();
+    	
+    	Socket socket= new Socket(serverAddress, 9040);
+    	in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    	out = new PrintWriter(socket.getOutputStream(), true);
+    	
+    	while(true) 
+    	{
+    		String line= in.readLine();
+    		if(line.startsWith("SUMBITNAME"))
+    		{
+    			out.println(getName());
+    		}
+    		
+    		else if (line.startsWith("NAMEACCEPTED"))
+    		{
+    			textField.setEditable(true);
+    		}
+    		else if (line.startsWith("MESSAGE"))
+    		{
+    			messageArea.append(line.substring(8)+"\n");
+    		}
+    	}
+    }
 	
-	public static void main(String[] argv)
+	public static void main(String[] argv) throws Exception
 	{
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		 FirstCLient client = new FirstCLient();
+		 client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	     client.frame.setVisible(true);
+	     client.run();
+	     
 	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 }
+
